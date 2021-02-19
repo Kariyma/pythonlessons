9cbf150af670c3d639d726626b055d1421b2b343
Mariya <mariika-sun@yandex.ru>
2021-02-19 05:58:11 +0300

    Completed task  1 from lesson 3

--- samples/lessons/03/pr3_1.py
+++ samples/lessons/03/pr3_1.py
@@ -1,7 +1,15 @@
 from random import *
 import turtle
 
-for i in range(50):
-    turtle.forward(randint(10, 200))
-    side = random()
-    turtle.
+turtle.Screen().setup(800, 800)
+turtle.shape('classic')
+turtle.color('yellow')
+turtle.pencolor('red')
+turtle.speed(10)
+turtle.pensize(3)
+
+while (True):
+    turtle.stamp()
+    turtle.right(randint(-360, 360))
+    turtle.forward(random()*100)
+
